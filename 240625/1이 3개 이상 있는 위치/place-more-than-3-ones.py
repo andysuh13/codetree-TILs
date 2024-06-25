ef510e3f53fd66b2a4b6f8e515047a77740b3b73
andysuh13 <100005321+andysuh13@users.noreply.github.com>
2024-06-25 15:43:08 +0900

    Title: 1이 3개 이상 있는 위치, Time: 135ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240625/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,24 @@
+n = int(input())
+maps = []
+for _ in range(n):
+    maps.append(list(map(int, input().split())))
+
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+result = 0
+for i in range(n):
+    for j in range(n):
+        cnt = 0
+        for k in range(4):
+            nr = i + dr[k]
+            nc = j + dc[k]
+            if nr < 0 or nr > 3 or nc < 0 or nc > 3:
+                continue
+
+            if maps[nr][nc] == 1:
+                cnt += 1
+        if cnt >= 3:
+            result += 1
+            
+print(result)
