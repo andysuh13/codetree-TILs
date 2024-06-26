37ffc5bcd0e829ad6cd0f5bd3f983c8f0389b0e8
andysuh13 <100005321+andysuh13@users.noreply.github.com>
2024-06-27 00:18:15 +0900

    Title: 모이자, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240627/모이자/gather.py
+++ 240627/모이자/gather.py
@@ -1,16 +1,3 @@
-# n = 5
-# arr = [1, 5, 2, 6, 8]
-# max_sum = 0
-
-# for i in range(n):
-#     arr[i] *= 2
-#     sum_diff = 0
-#     for j in range(n - 1):
-#         sum_diff += abs(arr[j + 1] - arr[j])
-#     max_sum = max(max_sum, sum_diff)
-#     arr[i] //= 2
-# print(max_sum)
-
 import sys
 
 n = int(input())
