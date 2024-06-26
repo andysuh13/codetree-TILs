d74f3a1449087e7b7f686f5ac6393ce73f087024
andysuh13 <100005321+andysuh13@users.noreply.github.com>
2024-06-27 00:58:23 +0900

    Title: 체크판위에서 2, Time: 126ms, Memory: 29MB, Status: Passed - CodeTree

--- 240627/체크판위에서 2/on-the-checkboard-2.py
+++ 240627/체크판위에서 2/on-the-checkboard-2.py
@@ -4,8 +4,8 @@ for _ in range(r):
     maps.append(input().split())
 
 cnt = 0
-for i in range(r):
-    for j in range(c):
+for i in range(1, r):
+    for j in range(1, c):
         for k in range(i + 1, r - 1):
             for l in range(j + 1, c - 1):
                 if (maps[0][0] != maps[i][j]) & (maps[i][j] != maps[k][l]) & (maps[k][l] != maps[r - 1][c - 1]):
